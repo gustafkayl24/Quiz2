961e83207eba5f407a8b7c2fb06682a9454997df
Kayla <Kayla@128.4.96.175>
2015-09-20 22:12:05 -0400

    Initial commit

---
+++ GustafsonQuiz2/src/Problem1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Problem1 {
+	public static void main(String[] args){
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Enter current tuition rate: ");
+		double tuitionCost = input.nextDouble();
+		
+		double TotalTuitionCost = tuitionCost * 4;  
+	}
+		
+		
+		
+	}
+
+	
+	
+
+
+
